086e46205be51733fd70982eac7fb3ea51d1fd2c
mwpodgorni <mwpodgorni@gmail.com>
2020-10-07 16:01:57 +0200

    project reupload

---
+++ MarkerlessARProject/Assets/Scripts/DogScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class DogScript : MonoBehaviour
+{
+    public GameObject dog;
+    public GameObject board;
+    
+    private GameObject spawnedObject1;
+    private GameObject spawnedObject2;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    
+    public AudioSource dogSound;
+    
+    private void Awake()
+    {
+        _arRaycastManager = GetComponent < ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) return;
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            if (spawnedObject1 == null && spawnedObject2 == null)
+            {
+                
+                spawnedObject1 = Instantiate(dog, hitPose.position, Quaternion.Euler(-90f,90f,0));
+                spawnedObject2 = Instantiate(board, hitPose.position+new Vector3(0.5f, 0,0), Quaternion.Euler(0,-90,0));
+            }
+            else
+            {
+                spawnedObject1.transform.position = hitPose.position;
+                spawnedObject2.transform.position = hitPose.position+new Vector3(0.5f, 0,0);
+            }
+        }
+    }
+    public void playSoundEffect()
+    {
+        dogSound.Play();
+    }
+}
